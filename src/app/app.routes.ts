3985b798cf7de1e1b749cb9aa18ce65c5a7f8be8
Alexandre Soler Sanandres <alexandre@soler-sanandres.net>
2020-09-14 18:11:16 +0200

    fix(app-routes.ts): update loadChildren

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import {Route} from '@angular/router';
 
+
 export const routes: Array<Route> = [
   {
     path: '',
-    loadChildren: './markdown/markdown.module#MarkdownModule',
+    loadChildren: () => import('./markdown/markdown.module#MarkdownModule').then(m => m.MarkdownModule),
   }
 ];
