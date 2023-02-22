41f08b3bd58dd372626ee56282b8d3004080eeea
Thorsten Brunzendorf <2067713-thbrunzendorf@users.noreply.gitlab.com>
2023-02-22 18:01:53 +0000

    feat: update to Angular 15 and migrate from ngx-md to ngx-markdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import {NgModule} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatButtonModule} from '@angular/material/button';
+import {MatLegacyButtonModule as MatButtonModule} from '@angular/material/legacy-button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
 import {AppComponent} from './app.component';
@@ -26,7 +26,7 @@ import {SponsorsModule} from './shared/sponsors/sponsors.module';
     MatSidenavModule,
     FlexLayoutModule,
     HttpClientModule,
-    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
+    RouterModule.forRoot(routes),
     SponsorsModule
   ],
   providers: [
